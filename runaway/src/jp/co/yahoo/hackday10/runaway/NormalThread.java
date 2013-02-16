6315ac3ce038632ca241fb97bf37e33aa20ffc3c
nb_mat <nob.mat@gmail.com>
2013-02-16 18:12:36 +0900

    add NormalMode

---
+++ runaway/src/jp/co/yahoo/hackday10/runaway/NormalThread.java
@@ -0,0 +1,26 @@
+package jp.co.yahoo.hackday10.runaway;
+
+import android.os.CountDownTimer;
+
+public class NormalThread extends CountDownTimer {
+	private MegControll mMegCon;
+	private boolean displayFlg = true;
+
+	public NormalThread(MegControll mc, int startTime) {
+		super(startTime, 1000);
+		mMegCon = mc;
+	}
+
+	public void onTick(long millisUntilFinished) {
+		if (displayFlg) {
+			mMegCon.countDownText(millisUntilFinished);
+		}
+	}
+
+	public void onFinish() {
+	}
+
+	public void displayStop() {
+		displayFlg = false;
+	}
+}
