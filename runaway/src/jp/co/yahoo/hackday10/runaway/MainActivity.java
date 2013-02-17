ca753ff28663bc5958e1210fbda1e1db0cd885b3
takehiroyano <atlas.yano@gmail.com>
2013-02-17 14:12:26 +0900

    とりあえず動いたバージョン

--- runaway/src/jp/co/yahoo/hackday10/runaway/MainActivity.java
+++ runaway/src/jp/co/yahoo/hackday10/runaway/MainActivity.java
@@ -3,29 +3,153 @@ package jp.co.yahoo.hackday10.runaway;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	final static private String TAG = MainActivity.class.getName();
 	
-	private Button mButton;
+	private ImageView mButton;
+	private ImageView mSplash;
+	private SeekBar mAreaSeek;
+	private SeekBar mHunterSeek;
+	private SeekBar mTimeSeek;
+	private TextView mAreaText;
+	private TextView mHunterText;
+	private TextView mTimeText;
+	private ImageView mWhite;
+	private long mTimes = 0;
+	final private static int[] AREA_SELECTS = {100, 300, 500, 1000, 5000};
+	final private static int[] HUNTER_SELECTS = {1, 5, 10, 20, 100};
+	final private static String[] TIME_SELECTS = {"30分", "1時間", "3時間", "1日", "無制限"};
+	final private static long[] TIME_SELECTS_LONG = {30*60, 60*60, 180*60, 24*60*60, 0};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "onCreate start");
 		setContentView(R.layout.main);
 		
-		mButton = (Button) findViewById(R.id.buttonStart);
+		mWhite = (ImageView) findViewById(R.id.imageWhite);
+		mButton = (ImageView) findViewById(R.id.buttonStart);
 		mButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Intent startIntent = new Intent(MainActivity.this, TimerActivity.class);
-				startActivity(startIntent);
+				AlphaAnimation animation = new AlphaAnimation(1, 0);
+				animation.setDuration(500);
+				animation.setAnimationListener(new Animation.AnimationListener() {
+					
+					@Override
+					public void onAnimationStart(Animation animation) {
+						mWhite.setVisibility(View.VISIBLE);
+					}
+					
+					@Override
+					public void onAnimationRepeat(Animation animation) {
+						;
+					}
+					
+					@Override
+					public void onAnimationEnd(Animation animation) {
+
+						Intent startIntent = new Intent(MainActivity.this, TimerActivity.class);
+						startIntent.putExtra("TIMES", mTimes);
+						startActivity(startIntent);
+						mWhite.setVisibility(View.GONE);
+					}
+				});
+				mWhite.startAnimation(animation);
 			}
 		});
+		mSplash = (ImageView) findViewById(R.id.imageSplash);
+		mAreaText = (TextView) findViewById(R.id.textArea);
+		mAreaSeek =(SeekBar) findViewById(R.id.seekArea);
+		mAreaSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				;
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				;
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				mAreaText.setText(AREA_SELECTS[progress] + "M");
+			}
+		});
+		mHunterText = (TextView) findViewById(R.id.textHunter);
+		mHunterSeek =(SeekBar) findViewById(R.id.seekHunter);
+		mHunterSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				;
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				;
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				mHunterText.setText(HUNTER_SELECTS[progress] + "人");
+			}
+		});
+		
+		mTimeText = (TextView) findViewById(R.id.textTime);
+		mTimeSeek =(SeekBar) findViewById(R.id.seekTime);
+		mTimeSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				;
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				;
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				mTimeText.setText(TIME_SELECTS[progress]);
+				mTimes = TIME_SELECTS_LONG[progress];
+			}
+		});
+		
+		mAreaSeek.setProgress(2);
+		mHunterSeek.setProgress(2);
+		mTimeSeek.setProgress(2);
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(500);
+					mGoneSplash.sendEmptyMessage(0);
+				} catch(Exception e) {
+					;
+				}
+			}
+		}).start();
+		
 	}
 
 	@Override
@@ -33,6 +157,36 @@ public class MainActivity extends Activity {
 		super.onResume();
 		Log.d(TAG, "onResume start");
 	}
+	
+	/**
+	 * スプラッシュ画像をフェードアウト
+	 */
+	Handler mGoneSplash = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			AlphaAnimation animation = new AlphaAnimation(1, 0);
+			animation.setDuration(1500);
+			animation.setAnimationListener(new Animation.AnimationListener() {
+				
+				@Override
+				public void onAnimationStart(Animation animation) {
+					;
+				}
+				
+				@Override
+				public void onAnimationRepeat(Animation animation) {
+					;
+				}
+				
+				@Override
+				public void onAnimationEnd(Animation animation) {
+					mSplash.setVisibility(View.GONE);
+				}
+			});
+			mSplash.startAnimation(animation);
+		}
+	};
 
 	@Override
 	protected void onPause() {
--- runaway/src/jp/co/yahoo/hackday10/runaway/TimerActivity.java
+++ runaway/src/jp/co/yahoo/hackday10/runaway/TimerActivity.java
@@ -6,30 +6,72 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.DigitalClock;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class TimerActivity extends Activity {
 	final static private String TAG = TimerActivity.class.getName();
 	
-	private TextView mTimer;
-	private TextView mRest;
+	private TextView mHour;
+	private TextView mSec;
+	private long mTimes;
+	private long mRestTime;
+	private LinearLayout mTimerLayout;
+	private ImageView mCountDown;
+	private ImageView mGiveUpButton;
+	private LinearLayout mBg;
 	private RunawayService mService;
+	private int mCount;
+	
+	final private static int[] COUNT_IMAGES = {R.drawable.countdown_1, R.drawable.countdown_2, R.drawable.countdown_3};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "onCreate start");
 		setContentView(R.layout.timer);
 		
-		mTimer = (TextView) findViewById(R.id.textTimer);
-		mRest = (TextView) findViewById(R.id.textRest);
+		mTimes = getIntent().getExtras().getLong("TIMES");
+		mHour = (TextView)findViewById(R.id.textHour);
+		mSec = (TextView)findViewById(R.id.textSec);
+		
+		mGiveUpButton = (ImageView) findViewById(R.id.buttonGiveUp);
+		mTimerLayout = (LinearLayout) findViewById(R.id.layoutTimer);
+		mCountDown = (ImageView) findViewById(R.id.imageCountDown);
+		mBg = (LinearLayout) findViewById(R.id.layoutBg);
+		
+		mGiveUpButton.setVisibility(View.GONE);
+		mGiveUpButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		mTimerLayout.setVisibility(View.GONE);
+		mBg.setVisibility(View.GONE);
+		mRestTime = mTimes;
+		mCount = 4;
 	}
 
-	//serviceConnection
+	/**
+	 * サービス接続
+	 */
 	ServiceConnection mConnection = new ServiceConnection() {
 		
 		@Override
@@ -55,17 +97,90 @@ public class TimerActivity extends Activity {
 		startService(serviceIntent);
 		//bind
 		bindService(serviceIntent, mConnection, Context.BIND_AUTO_CREATE);
+		
+		dispCount(3);
 	}
+	
+	/**
+	 * カウントダウン実施
+	 * @param cnt
+	 */
+	private void dispCount(final int cnt) {
+		if (cnt == 0) {
+			//カウントダウン終了時
+			mBg.setVisibility(View.VISIBLE);
+			mTimerLayout.setVisibility(View.VISIBLE);
+			mGiveUpButton.setVisibility(View.VISIBLE);
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					while (mRestTime >= 0) {
+						drawHandler.sendEmptyMessage(0);
+						try {
+							Thread.sleep(1000);
+						} catch (Exception e) {
+							;
+						}
+						mRestTime--;
+					}
+				}
+			}).start();
+			return;
+		}
+		AnimationSet animationSet = new AnimationSet(true);
+		AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
+		ScaleAnimation scaleAnimation = new ScaleAnimation((float)1.5, (float)0.8, (float)1.5, (float)0.8);
+		animationSet.addAnimation(alphaAnimation);
+		animationSet.addAnimation(scaleAnimation);
+		animationSet.setDuration(999);
+		animationSet.setAnimationListener(new Animation.AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation animation) {
+				Log.d("ANIME", "onAnimationStart");
+				mCountDown.setImageResource(COUNT_IMAGES[cnt-1]);
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				;
+			}
+			
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				Log.d("ANIME", "onAnimationEnd");
+				mCountDown.setVisibility(View.GONE);
+				dispCount(cnt - 1);
+			}
+		});
+		mCountDown.setVisibility(View.VISIBLE);
+		mCountDown.startAnimation(animationSet);
+	}
+	
+	/**
+	 * タイマー描画
+	 */
+	private Handler drawHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			Log.d("TimerActivity", "call drawHandler:" + mCount + "/" + mRestTime);
+			mHour.setText(Integer.toString((int)(mRestTime / 60)));
+			mSec.setText(String.format("%02d", mRestTime % 60));
+		};
+	};
 
 	@Override
 	protected void onPause() {
 		super.onPause();
 		Log.d(TAG, "onPause start");
+		unbindService(mConnection);
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
+		mRestTime = 0;
+		mCount = 0;
 		Log.d(TAG, "onDestroy start");
 	}
 }
